5ab289d25843d37cb405479d45b11f0eeb4799fd
Bruno Daniel <frandalozzo@gmail.com>
2021-09-14 17:07:13 -0300

    add
    add

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,41 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+//import CanFlag from '../../assets/images/canada.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'Canadá',
+    value: 'canada',
+   // flag: CanFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  }
+]
+
+export default COUNTRIES
